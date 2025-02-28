7cf306ddb6404efdeef58296f46840a56fe9dc3f
Nick McCullough <nickmcc@iastate.edu>
2025-02-27 22:11:12 -0600

    added vertical timeline to Experience section

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,79 +1,188 @@
-import johndeereLogo from "./johndeere.jpg";
-import collinsLogo from "./collinsaero.jpg";
+// import johndeereLogo from "./johndeere.jpg";
+// import collinsLogo from "./collinsaero.jpg";
+
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import ExperienceCard from "./ExperienceCard";
+// import Particle from "../Particle";
+
+
+
+// function Experience() {
+//   return (
+//     <Container fluid className="experience-section project-section">
+//       <Particle />
+//       <Container>
+//         <h1 className="experience-heading">
+//           <strong className="darker-spiritbox">
+//             Professional Software Engineering Experience
+//           </strong>
+//           <br></br><br></br>
+//           <small style={{ fontSize: "0.90rem" }}>
+//             <p className="lighter-spiritbox">To support myself during my ISU undergraduate studies, I worked at
+//             least 20 hours per week in all but two semesters.
+//             <br></br>
+//             <br></br>
+//             In three semesters, I balanced a full-time course load while working
+//             40 hours per week.</p>
+//           </small>
+//         </h1>
+
+//         <Row className="row-cols-1 row-cols-md-2 g-4">
+//           <Col className="experience-card">
+//             <ExperienceCard
+//               imgPath={collinsLogo}
+//               company="Collins Aerospace / RTX"
+//               position="Software Engineer Intern"
+//               duration="Summer 2025"
+//               team="Military Avionics Performance Software"
+//             />
+//           </Col>
+
+//           <Col className="experience-card">
+//             <ExperienceCard
+//               imgPath={collinsLogo}
+//               company="Collins Aerospace / RTX"
+//               position="Software Engineer, Part-Time during my senior year"
+//               duration="Aug 2024 - Apr 2025"
+//               team="Military Avionics & Helicopter Engineering"
+//             />
+//           </Col>
+
+//           <Col className="experience-card">
+//             <ExperienceCard
+//               imgPath={collinsLogo}
+//               company="Collins Aerospace / RTX"
+//               position="Software Engineer Co-Op"
+//               duration="Jan 2024 - August 2024"
+//               team="Military Avionics Navigation & Guidance Software"
+//             />
+//           </Col>
+
+//           <Col className="experience-card">
+//             <ExperienceCard
+//               imgPath={johndeereLogo}
+//               company="John Deere"
+//               position="Software Engineer Intern"
+//               duration="July 2022 - Apr 2023"
+//               team="Baler Product Engineering Team"
+//             />
+//           </Col>
+//         </Row>
+//         <br></br><br></br>
+//         <strong className="lighter-spiritbox"> <small style={{ fontSize: "0.90rem" }}>
+//           *Collins Aerospace is a subsidiary of RTX Corporation (Raytheon
+//           Technologies)
+//         </small> </strong>
+//       </Container>
+//     </Container>
+//   );
+// }
+
+// export default Experience;
+
+
+// --------------------------------------------------------------
 
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ExperienceCard from "./ExperienceCard";
+import { Container } from "react-bootstrap";
 import Particle from "../Particle";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
+import ExperienceCard from "./ExperienceCard";
+import collinsLogo from "./collinsaero.jpg";
+import johndeereLogo from "./johndeere.jpg";
 
+// Experience Data
+const experiences = [
+  {
+    id: 1,
+    imgPath: collinsLogo,
+    company: "Collins Aerospace / RTX",
+    position: "Software Engineer Intern",
+    duration: "Summer 2025",
+    team: "Military Avionics Performance Software",
+  },
+  {
+    id: 2,
+    imgPath: collinsLogo,
+    company: "Collins Aerospace / RTX",
+    position: "Software Engineer, Part-Time senior year",
+    duration: "Aug 2024 - Apr 2025",
+    team: "Military Avionics & Helicopter Engineering",
+  },
+  {
+    id: 3,
+    imgPath: collinsLogo,
+    company: "Collins Aerospace / RTX",
+    position: "Software Engineer Co-Op",
+    duration: "Jan 2024 - August 2024",
+    team: "Military Avionics Navigation Software",
+  },
+  {
+    id: 4,
+    imgPath: johndeereLogo,
+    company: "John Deere",
+    position: "Software Engineer Intern",
+    duration: "July 2022 - Apr 2023",
+    team: "Baler Product Engineering Team",
+  },
+];
 
 function Experience() {
   return (
     <Container fluid className="experience-section project-section">
       <Particle />
       <Container>
-        <h1 className="experience-heading">
+        <h1 className="experience-heading lighter-spiritbox">
           <strong className="darker-spiritbox">
             Professional Software Engineering Experience
           </strong>
-          <br></br><br></br>
+          <br />
+          <br />
           <small style={{ fontSize: "0.90rem" }}>
-            <p className="lighter-spiritbox">To support myself during my ISU undergraduate studies, I worked at
-            least 20 hours per week in all but two semesters.
-            <br></br>
-            <br></br>
-            In three semesters, I balanced a full-time course load while working
-            40 hours per week.</p>
+            <p className="lighter-spiritbox">
+              To support myself during my ISU undergraduate studies, I worked at
+              least 20 hours per week in all but two semesters.
+              <br />
+              <br />
+              In three semesters, I balanced a full-time course load while
+              working 40 hours per week.
+            </p>
           </small>
         </h1>
 
-        <Row className="row-cols-1 row-cols-md-2 g-4">
-          <Col className="experience-card">
-            <ExperienceCard
-              imgPath={collinsLogo}
-              company="Collins Aerospace / RTX"
-              position="Software Engineer Intern"
-              duration="Summer 2025"
-              team="Military Avionics Performance Software"
-            />
-          </Col>
+        {/* Vertical Timeline */}
+        <VerticalTimeline>
+          {experiences.map((exp, index) => (
+            <VerticalTimelineElement
+              key={exp.id}
+              date={
+                <div className="timeline-date-container">
+                  <span className="timeline-date lighter-spiritbox">
+                    {exp.duration}
+                  </span>
+                  <div className="timeline-date-line"></div>
+                  {/* Add this line */}
+                </div>
+              }
+              iconStyle={{ background: "#563e5f", color: "#fff" }}
+              position={index % 2 === 0 ? "left" : "right"}
+            >
+              <ExperienceCard {...exp} />
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
 
-          <Col className="experience-card">
-            <ExperienceCard
-              imgPath={collinsLogo}
-              company="Collins Aerospace / RTX"
-              position="Software Engineer, Part-Time during my senior year"
-              duration="Aug 2024 - Apr 2025"
-              team="Military Avionics & Helicopter Engineering"
-            />
-          </Col>
-
-          <Col className="experience-card">
-            <ExperienceCard
-              imgPath={collinsLogo}
-              company="Collins Aerospace / RTX"
-              position="Software Engineer Co-Op"
-              duration="Jan 2024 - August 2024"
-              team="Military Avionics Navigation & Guidance Software"
-            />
-          </Col>
-
-          <Col className="experience-card">
-            <ExperienceCard
-              imgPath={johndeereLogo}
-              company="John Deere"
-              position="Software Engineer Intern"
-              duration="July 2022 - Apr 2023"
-              team="Baler Product Engineering Team"
-            />
-          </Col>
-        </Row>
-        <br></br><br></br>
-        <strong className="lighter-spiritbox"> <small style={{ fontSize: "0.90rem" }}>
-          *Collins Aerospace is a subsidiary of RTX Corporation (Raytheon
-          Technologies)
-        </small> </strong>
+        <br />
+        <br />
+        <strong className="lighter-spiritbox">
+          <small style={{ fontSize: "0.90rem" }}>
+            *Collins Aerospace is a subsidiary of RTX Corporation (Raytheon
+            Technologies)
+          </small>
+        </strong>
       </Container>
     </Container>
   );
--- src/Components/Experience/ExperienceCard.js
+++ src/Components/Experience/ExperienceCard.js
@@ -1,20 +1,49 @@
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+
+// function ExperienceCard(props) {
+//   return (
+//     <Card className="project-card-view">
+//       <Card.Img variant="top" src={props.imgPath} alt="company-img" />
+//       <Card.Body>
+//         <Card.Title>{props.company}</Card.Title>
+//         <Card.Text style={{ textAlign: "justify" }}>
+//           <strong>Position:</strong> {props.position}
+          
+//           <br />
+//           <strong>Team:</strong> {props.team}
+//           <br />
+//           <strong>Duration:</strong> {props.duration}
+        
+//         </Card.Text>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default ExperienceCard;
+
+// --------------------------------------------------------------
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-function ExperienceCard(props) {
+function ExperienceCard({ imgPath, company, position, team }) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="company-img" />
+    <Card className="experience-card-view">
+      <div className="experience-logo-container">
+        <Card.Img
+          variant="top"
+          src={imgPath}
+          alt={`${company} logo`}
+          className="experience-logo"
+        />
+      </div>
       <Card.Body>
-        <Card.Title>{props.company}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          <strong>Position:</strong> {props.position}
-          
+        <Card.Title className="lighter-spiritbox">{company}</Card.Title>
+        <Card.Text className="lighter-spiritbox">
+          <strong>Position:</strong> {position}
           <br />
-          <strong>Team:</strong> {props.team}
-          <br />
-          <strong>Duration:</strong> {props.duration}
-        
+          <strong>Team:</strong> {team}
         </Card.Text>
       </Card.Body>
     </Card>
